d76424743c3aa7c9ad1a643affc935428ceafff5
Abhishek Patel <patelabhishek1811@gmail.com>
2025-10-10 11:05:10 +0530

    Multiple route handlers

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,57 +2,26 @@ const express = require("express");
 
 const app = express();
 
-// /ac , /abc
-/* app.get("/ab?c", (req, res) => {
-  console.log(req.query);
-  res.send({ firstName: "Abhishek", lastName: "Patel" });
-});
-
-app.get("/ab+c", (req, res) => {
-  console.log(req.query);
-  res.send({ firstName: "Abhishek", lastName: "Patel" });
-});
-
-app.get("/ab*cd", (req, res) => {
-  console.log(req.query);
-  res.send({ firstName: "Abhishek", lastName: "Patel" });
-});
-
-//Optional
-app.get("/a(bc)?d", (req, res) => {
-  console.log(req.query);
-  res.send({ firstName: "Abhishek", lastName: "Patel" });
-});
-
-app.get("/a(bc)+d", (req, res) => {
-  console.log(req.query);
-  res.send({ firstName: "Abhishek", lastName: "Patel" });
-});
-
- */
-
-// Regex
-app.get(/a/, (req, res) => {
-  console.log(req.query);
-  res.send({ firstName: "Abhishek", lastName: "Patel" });
-});
-
-app.get(/.*fly$/, (req, res) => {
-  console.log(req.query);
-  res.send({ firstName: "Abhishek", lastName: "Patel" });
-});
-
-// query paramters
-app.get("/user", (req, res) => {
-  console.log(req.query);
-  res.send({ firstName: "Abhishek", lastName: "Patel" });
-});
-
-// Dynamic routes params
-app.get("/user/:userId/:name", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Abhishek", lastName: "Patel" });
-});
+// Multiple route handlers
+app.use(
+  "/user",
+  (req, res, next) => {
+    // Route handler 1
+    next();
+    // res.send("Route handler response 1");
+  },
+  [
+    (req, res, next) => {
+      // Route handler 2
+      // res.send("Route handler response 2");
+      next();
+    },
+    (req, res, next) => {
+      // Route handler 3
+      res.send("Route handler response 3");
+    },
+  ]
+);
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000...");
