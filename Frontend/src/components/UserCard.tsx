c65849701f20384b556e38062d24dbfefa56473b
Abhishek Patel <patelabhishek1811@gmail.com>
2026-01-04 00:51:18 +0530

    Improved the user card to have a fix height and width when no user image

--- Frontend/src/components/UserCard.tsx
+++ Frontend/src/components/UserCard.tsx
@@ -1,5 +1,4 @@
 
-import { Check, X } from "lucide-react";
 
 export const UserCard = ({ user }: { user: any }) => {
     return (
@@ -10,9 +9,9 @@ export const UserCard = ({ user }: { user: any }) => {
                         <div key={item?._id || `user-${index}`}>
                             <div className="aspect-16/20 relative flex items-center justify-center overflow-hidden rounded-lg">
                                 {item?.photoUrl ? (
-                                    <img src={item.photoUrl} alt={item?.firstName || "User"} className="rounded-lg object-cover size-full" />
+                                    <img src={item.photoUrl} alt={item?.firstName || "User"} className="rounded-lg object-cover object-top size-full" />
                                 ) : (
-                                    <div className="rounded-lg object-cover size-full bg-gray-200 flex items-center justify-center text-gray-400">
+                                    <div className="rounded-lg object-cover size-full bg-gray-200 flex items-center justify-center text-gray-400 w-96 h-120">
                                         No Photo
                                     </div>
                                 )}
@@ -20,22 +19,26 @@ export const UserCard = ({ user }: { user: any }) => {
                                 </div>
                                 <div className="absolute bottom-0 left-0 right-0 px-4 py-6 space-y-1 text-white">
                                     <h2 className="text-3xl font-semibold tracking-wide">{item?.firstName} <span className="font-extralight">{item?.age}</span></h2>
-                                    <p className="text-sm line-clamp-2">{item?.about}</p>
+                                    <p title={item?.about} className="text-sm line-clamp-2">{item?.about}</p>
                                     <div className="flex items-center gap-2">
-                                        <span className="text-sm">Skills:{" "}<span className="font-bold">
-                                            {Array.isArray(item?.skills)
-                                                ? item.skills.join(", ")
-                                                : (item?.skills || "No skills listed")}
-                                        </span></span>
+                                        {/* Show upto only 2 skills */}
+                                        <span className="text-sm">Skills:{" "}
+                                            <span className="font-bold">
+                                                {Array.isArray(item?.skills)
+                                                    ? item.skills.slice(0, 2).join(", ")
+                                                    : (item?.skills || "No skills listed")}
+                                                {item.skills.length > 2 && <span className="text-xs text-gray-500">...</span>}
+                                            </span>
+                                        </span>
                                     </div>
-                                    <div className="flex items-center justify-end gap-2 mt-4">
+                                    {/* <div className="flex items-center justify-end gap-2 mt-4">
                                         <button title="Like" className="cursor-pointer flex items-center justify-center size-10 bg-white rounded-full">
                                             <Check className="size-8 text-green-500" strokeWidth={3} />
                                         </button>
                                         <button title="Dislike" className="cursor-pointer flex items-center justify-center size-10 bg-white rounded-full">
                                             <X className="size-8 text-red-500" strokeWidth={3} />
                                         </button>
-                                    </div>
+                                    </div> */}
                                 </div>
                             </div>
                         </div>
